da8870f85bc75451d0eb1d6773a3a645225ab626
Nahomie ND <nducasse3@gmail.com>
2020-12-16 22:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var katzDeliLine =[];
 
 function takeANumber(katzDeliLine, name) {
@@ -27,4 +28,46 @@ for (let i = 0; i < katzDeliLine.length; i++) {
   }
   
   return `The line is currently: ${numbersAndNames.join(', ')}`
+=======
+var katzDeli =[];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return katzDeliLine
+}
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number 1 in line.`
+}
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine, name){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = katzDeliLine[0];
+    katzDeliLine.splice(0,1);
+    return "Currently serving " + name + ".";
+  }
+}
+
+var line = 0;
+
+function currentLine(katzDeli){
+  if(katzDeli.length === 0){
+    return "The line is currently empty."
+  } else{
+    var response = 'The line is currently:'
+     for (var i=0; i<katzDeli.length-1; i++){
+       response += " " + Number(i+1) + ". " + katzDeli[i] + ","
+          }
+    response += " " + katzDeli.length + ". " + katzDeli[katzDeli.length-1]
+     return response;
+   }
+>>>>>>> 7799b64a373e9f057b2e0f2890b00d68eb6b6820
 }
